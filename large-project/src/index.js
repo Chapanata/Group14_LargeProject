d0a128b7c3653fc339370b3158ab41b460f7eb6f
Chapanata <krischoudhury@knights.ucf.edu>
2020-07-01 08:57:00 -0400

    Finished Styling

--- large-project/src/index.js
+++ large-project/src/index.js
@@ -79,16 +79,21 @@ class LoginBox extends React.Component {
       <div className="box">
 
         <div className="input-group">
-          
+          <label className="login-label">E-Mail Address:</label>
           <input type="email" name="e-mail" className="login-input" placeholder="E-Mail Address"/>
         </div>
 
         <div className="input-group">
-          
+          <label className="login-label">Password:</label>
           <input type="password" name="password" className="register-input" placeholder="Password"/>
         </div>
 
-        <button type="button" className="login-button" onClick={this.submitLogin.bind(this)}>Login</button>
+        <button type="button" className="login-button" onClick={this.submitLogin.bind(this)}>LOGIN</button>
+      
+        <div className="seperator"> or </div>
+
+        <button type="button" className="register-button" onClick={this.submitLogin.bind(this)}>REGISTER</button>
+
       </div>      
     </div>
     );
@@ -120,19 +125,23 @@ class RegisterBox extends React.Component {
       <div className="box">
 
         <div className="input-group">
-          
+          <label className="login-label">Enter an E-Mail Address:</label>
           <input type="email" name="e-mail" className="login-input" placeholder="E-Mail Address"/>
         </div>
 
         <div className="input-group">
+          <label className="login-label">Enter Password</label>
           <input type="password" name="password" className="register-input" placeholder="Password"/>
         </div>
 
         <div className="input-group">
+          <label className="login-label">Confirm Password:</label>
           <input type="password" name="password" className="register-input" placeholder="Confirm Password"/>
         </div>
 
-        <button type="button" className="register-button" onClick={this.submitRegister.bind(this)}>Register</button>
+        <button type="button" className="register-button" onClick={this.submitRegister.bind(this)}>REGISTER</button>
+      
+        <div className="backToLog">Already have an Account? <a href="#">Click Here</a></div>
       </div>
     </div>
     );
