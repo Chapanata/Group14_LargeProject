c439e62543680248bc64b46f37fedd85ec24bc7b
Sammie Chou <sammie_chou@Sammies-MBP.attlocal.net>
2020-07-02 06:23:55 -0400

    Design and functionality changes

--- large-project/src/App.js
+++ large-project/src/App.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import { BrowserRouter, Route } from 'react-router-dom';
+
 import Home from "./Pages/Home/Home";
 import Login from "./Pages/Login/Login";
+import Register from "./Pages/Login/Register";
+import About from "./Pages/Dashboard/About";
+import Contact from "./Pages/Dashboard/Contact";
 
 class App extends Component {
   render() {
@@ -10,7 +14,11 @@ class App extends Component {
       < BrowserRouter>
         <div className="App">
           <Route path="/" exact component={Home} />
+          <Route path="/Home" exact component={Home} />
           <Route path="/Login" exact component={Login} />
+          <Route path="/Register" exact component={Register} />
+          <Route path="/About" exact component={About} />
+          <Route path="/Contact" exact component={Contact} />
         </div> 
       </BrowserRouter>
     );
