078b0924943a8c6a76878a70da59cb9a4a376416
Chapanata <krischoudhury@knights.ucf.edu>
2020-07-03 16:54:56 -0400

    Removed Slider from Login

--- large-project/src/Pages/Login/Login.js
+++ large-project/src/Pages/Login/Login.js
@@ -9,6 +9,10 @@ class Login extends Component {
       this.state = { isLoginOpen: true, isRegisterOpen: false };
     }
 
+    submitLogin(e) {
+  
+    }
+
     showLoginBox() {
       this.setState({isLoginOpen: true, isRegisterOpen: false});
     }
@@ -20,74 +24,39 @@ class Login extends Component {
     render() {
       return (
         <div className="page">
-        <div className="root-container">
-            <div className="box-slider">
-  
-            <div className="slider" onClick={this.showLoginBox.bind(this)}>
-              Login
+          <div className="root-container">
+            <div className="box-container">
+      
+              <img src={logo} className="Logo" />
+
+              <div className="title">
+                    Login  
+              </div>
+              <div className="box">
+
+                <div className="input-group">
+                  <label className="login-label">E-Mail Address:</label>
+                  <input type="email" name="e-mail" className="login-input" placeholder="E-Mail Address"/>
+                </div>
+
+                <div className="input-group">
+                  <label className="login-label">Password:</label>
+                  <input type="password" name="password" className="register-input" placeholder="Password"/>
+                </div>
+
+                <button type="button" className="login-button" onClick={this.submitLogin.bind(this)}>LOGIN</button>
+              
+                <div className="seperator"> or </div>
+
+                <button type="button" className="register-button" onClick={this.submitLogin.bind(this)}>REGISTER</button>
+
+              </div>      
             </div>
-            <div className="slider" onClick={this.showRegisterBox.bind(this)}>
-              Register
-            </div>
-  
-        {/*         
-            <button type="button" className="slider" onClick={this.showLoginBox.bind(this)}>Login</button>
-            <button type="button" className="slider" onClick={this.showRegisterBox.bind(this)}>Register</button>
-             */}
           </div>
-  
-          <div className="box-container">
-  
-            {this.state.isLoginOpen && <LoginBox />}
-  
-          </div>
-        </div>
         </div>
       );
     }
 }
   
-class LoginBox extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {  };
-    }
-  
-    submitLogin(e) {
-  
-    }
-  
-    render() {
-      return(
-      <div className="inner-container">
-  
-        <img src={logo} className="Logo" />
-  
-        <div className="title">
-              Login  
-        </div>
-        <div className="box">
-  
-          <div className="input-group">
-            <label className="login-label">E-Mail Address:</label>
-            <input type="email" name="e-mail" className="login-input" placeholder="E-Mail Address"/>
-          </div>
-  
-          <div className="input-group">
-            <label className="login-label">Password:</label>
-            <input type="password" name="password" className="register-input" placeholder="Password"/>
-          </div>
-  
-          <button type="button" className="login-button" onClick={this.submitLogin.bind(this)}>LOGIN</button>
-        
-          <div className="seperator"> or </div>
-  
-          <button type="button" className="register-button" onClick={this.submitLogin.bind(this)}>REGISTER</button>
-  
-        </div>      
-      </div>
-      );
-    }
-}
 
 export default Login;
