25528466e60a3003ba0c60ff5b05bdf2a296861d
Chapanata <krischoudhury@knights.ucf.edu>
2020-07-15 18:05:24 -0400

    Changed Styling of Landing Page

--- large-project/src/Pages/Home/Home.js
+++ large-project/src/Pages/Home/Home.js
@@ -17,31 +17,28 @@ class Home extends Component {
     render() {
         return(
             <div className="in-container">
-                <nav>
-                    <div className="nav-wrapper">
-                        <ul>
-                        <li><Link to="/Home">Home</Link></li>
-                        <li><Link to="/About">About Us</Link></li>
-                        <li><Link to="/Contact">Contact Us</Link></li>
-                        </ul>
-                        
+                <div className="nav-bar">
+                    <div className="nav-centered">
+                        {/* Replace href with Link later */}
+                        <a href="#"><Link to="/Home">Home</Link></a>
                     </div>
-                </nav>
+
+                    <a href="#"><Link to="/About">About Us</Link></a>
+                    <a href="#"><Link to="/Contact">Contact Us</Link></a>
+
+                    <div class="nav-right">
+                        <a href="#" ><Link to="/Login">Login</Link></a>
+                        <a href="#"><Link to="/Register">Register</Link></a>
+                    </div>
+                </div>
+
                 <div className="headerBox">
                     <div className="left-box">
                         <img src={logo} className="logo" />
-                    </div>
-                    <div className="right-box">               
-                        <div>
-                            <button type="button" className="login-button" onClick={this.submitLogin.bind(this)}><Link to="/Register">Create a free account!</Link></button>
-                        </div>
-                        <div>
-                            <Link to="/login">Already have an account with us? Login!</Link>
-                        </div>
-                    </div>
+                    </div>  
                  </div>
 
-                 <div className="body">
+                 {/* <div className="body">
                      <div> . </div>
                      <div> . </div>
                      <div>
@@ -68,7 +65,7 @@ class Home extends Component {
                      </div>
                      <div> . </div>
                      <div> . </div>
-                 </div>
+                 </div> */}
 
                  <Link to="/Settings">Settings</Link>
                  <br></br>
