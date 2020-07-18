3356788662297034becdff862af6723887c0f3f3
Chapanata <krischoudhury@knights.ucf.edu>
2020-07-18 18:17:30 -0400

    Added User Input for Bio Page

--- large-project/src/Pages/User-Pages/Biometrics.js
+++ large-project/src/Pages/User-Pages/Biometrics.js
@@ -10,6 +10,115 @@ class Biometrics extends Component {
         
     }
 
+    state = {
+        weight: "0",
+        gender: "Choose Gender",
+        feet: "0",
+        inches: "0",
+        buttonText: "0",
+        BMI: "0",
+        isInEditMode: false,
+        buttonMode: false
+    }
+    changeEditMode = () => {
+        this.setState({
+            isInEditMode: !this.state.isInEditMode
+        })
+        console.log("This works");
+    }
+
+    updateComponentValues = () => {
+        this.setState({
+            isInEditMode: false,
+            weight: this.state.weight
+        })
+    }
+
+
+    renderDefaultView = () => {
+        return <div className="biogrid-container1">
+        <h3>Weight: {this.state.weight} pounds</h3>
+        <h3>Gender: {this.state.gender}</h3>
+        <h3>Height: {this.state.feet} feet {this.state.inches} inches</h3>
+        <h3>BMI: {this.state.BMI}</h3>
+        </div>
+        
+    }
+
+    renderEditView = () => {
+        return <div class="biogrid-container1">
+        <h3>Weight: 
+        {
+            this.state.isInEditMode 
+            ? 
+                <input type="text" defaultValue={this.state.weight} ref="weightInput"/>
+              
+              
+            : <div>
+                {this.state.weight}
+              </div>
+
+        } pounds</h3>
+
+        <h3>Gender:
+        {
+            this.state.isInEditMode
+            ?
+                <select name="genderChoice">
+                    <option value="Male">Male</option>
+                    <option value="Female">Female</option>
+                </select>
+            
+            : <div>
+                {this.state.gender}
+            </div>
+        }
+
+        </h3>
+
+        <h3>Height:
+        {
+            this.state.isInEditMode
+            ? 
+                <input type="text" ref = "feetInput" defaultValue={this.state.feet}/>
+            
+            : <div className="feet">
+                {this.state.feet};
+            </div>
+                
+        }
+        Feet 
+        {
+            this.state.isInEditMode
+            ? 
+                <input type="text" ref = "inchesInput" defaultValue={this.state.inches}/>
+            
+            : <div className="inches">
+                {this.state.inches};
+            </div>
+                
+        }
+        Inches
+        </h3>
+
+        <h3>BMI: 
+        {
+            this.state.isInEditMode 
+            ? 
+                <input type="text" ref = "BMIinput" defaultValue={this.state.BMI} />
+              
+              
+            : <div>
+                {this.state.BMI}
+              </div>
+
+        } 
+        </h3>
+        <button type="button" class="saveStats" onClick={this.updateComponentValues}>Save Changes</button>
+        </div>
+    }
+
+
     render() {
 
         return (
@@ -39,17 +148,18 @@ class Biometrics extends Component {
                         <div className="bio-container2">
                         <h2>Your Statistics</h2>
 
+                        {/* Will Contain Render Edits */}
                             <div class="biogrid-container1">
-                                <h3>Weight:# pounds</h3>
-                                <h3>Gender: (Dropdown)</h3>
-                                <h3>Height: # feet # inches</h3>
+                                {
+                                    this.state.isInEditMode 
+                                    ? this.renderEditView()
+                                    : this.renderDefaultView()
+
+                                }
                             </div>
 
-                            <div class="biogrid-container2">
-                                <h2>BMI:</h2>
-                                <h2> #</h2>
-                            </div>
-                            <button type="button" class="changeStats">Make Changes</button>
+                            
+                            <button type="button" class="changeStats" onClick={this.changeEditMode}>Make Changes</button>
                         </div>
 
                         <div className="bio-container3">
