4056901872af88e1c57c8070a7457658699d32e1
Samuel <armi.sam99@gmail.com>
2020-07-20 15:32:22 -0400

    + Updated consumed and package

--- ExpressServer/app/models/Consumed.js
+++ ExpressServer/app/models/Consumed.js
@@ -1,13 +1,14 @@
-// app/models/bear.js
+// app/models/consumed.js
 
 var mongoose     = require('mongoose');
 var Schema       = mongoose.Schema;
 
-var BearSchema   = new Schema({
-    UserID:[
-    {type: Schema.Types.ObjectId, ref: 'User'}
-  ],
-    foodID: Int32Array
+var ConsumedSchema   = new Schema({
+    UserID: {
+      type: Schema.Types.ObjectId, ref: 'User'},
+    FDCID: {
+      type: Number
+    }
 });
 
 module.exports = mongoose.model('Consumed', BearSchema);
