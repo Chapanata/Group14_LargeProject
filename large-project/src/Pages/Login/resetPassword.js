6b09729a33c4d62a953d5052979acd45fd4029e2
Sammie Chou <sammie_chou@Sammies-MacBook-Pro.local>
2020-07-24 16:55:28 -0400

    reverting changes

--- large-project/src/Pages/Login/resetPassword.js
+++ large-project/src/Pages/Login/resetPassword.js
@@ -9,136 +9,114 @@ var pass="";
 var confirm="";
 var pageURL = window.location.href;
 
-/*
 var heroku = 'https://nutrition-heroku.herokuapp.com/resetPassword';
 
 var keyValue = pageURL.substring(60);
 
 var finalURL = heroku.concat(keyValue);
-*/
+
 function comparePass(value1, value2) {
 
-  if(pass === confirm) {
-    return true;
-  }
-  else {
-    return false;
-  }
+if(pass === confirm) {
+  return true;
 }
-
+else {
+  return false;
+}
+}
 const formValid = ({ errors, ...rest }) => {
-  let valid = true;
-
-  Object.values(errors).forEach(val => {
-    val.length > 0 && (valid = false);
+let valid = true;
+Object.values(errors).forEach(val => {
+  val.length > 0 && (valid = false);
 });
-
-  Object.values(rest).forEach(val => {
-    val === null && (valid = false)
-  });
-
-  return valid;
+Object.values(rest).forEach(val => {
+  val === null && (valid = false)
+});
+return valid;
 };
-
 class resetPassword extends Component {
-
-    constructor(props) {
-      super(props);
-      this.state = {
-        password: null, 
-        confirmPassword: null, 
-        errors: {
-          password:"", 
-          confirmPassword:"",
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      password: null, 
+      confirmPassword: null, 
+      errors: {
+        password:"", 
+        confirmPassword:"",
       }
     }
-
-    state = {
-      apiError: '',
-      apiSuccess: '',
-      successMessage: ''
-    }
-
-    handleSubmit = e => {
-      e.preventDefault();
-
-      if (formValid(this.state)) {
-        console.log(`
-          -- Submitting --
-          Password: ${this.state.password}    
-        `);
-
-        var email;
-        var confirmCode;
-
-        // http://localhost:8080/resetPassword
-        // https://nutrition-heroku.herokuapp.com/resetPassword
-        axios.post(pageURL, 
-        { password: this.state.password },
-        { params: {email, confirmCode}}
-        )
-        .then(response => {
-            console.log(response.data)
-            console.log(response.data.Success)
-            console.log(response.data.Error)
-            this.setState({
-                apiError: response.data.Error,
-                apiSuccess: response.data.Success
-            })
-        })
-        .catch(error =>{
-            console.log(error.response)
-        })
-        window.location.assign("https://nutrition-heroku.herokuapp.com/login")
-      } 
-      
-      else {
-        console.error('FORM INVALID');
-      }
-    }
-
+  }
+  state = {
+    apiError: '',
+    apiSuccess: '',
+    successMessage: ''
+  }
+  
+  
+  handleSubmit = e => {
+    e.preventDefault();
+    if (formValid(this.state)) {
+      console.log(`
+        -- Submitting --
+        Password: ${this.state.password}    
+      `);
+      // http://localhost:8080/resetPassword
+      // https://nutrition-heroku.herokuapp.com/resetPassword
+      axios.post(finalURL, 
+      { password: this.state.password })
+      .then(response => {
+          console.log(response.data)
+          console.log(response.data.Success)
+          console.log(response.data.Error)
+          this.setState({
+              apiError: response.data.Error,
+              apiSuccess: response.data.Success
+          })
+      })
+      .catch(error =>{
+          console.log(error.response)
+      })
+      window.location("/login")
+    } 
     
-    handleChange = e => {
-
-      
-
-      e.preventDefault();
-      const { name, value } = e.target;
-      let errors = this.state.errors;
-
-      
-      switch (name) {
-
-        case 'password':
-          pass = value;
-          if (passRegex.test(value) != true) {
-            errors.password = "Minimum 6 characters required with at least 1 number, 1 letter, and 1 special character";
-          }
-          
-          else {
-            errors.password ="";
-          }
-      
-        break;
-
-        case 'confirmPassword':
-          confirm = value;
-          if (confirm !== pass){
-            errors.confirmPassword = "Passwords do not match";
-          }
-
-          else {
-            errors.confirmPassword = "";
-          }
-
-        break;
-
-        default:
+    else {
+      console.error('FORM INVALID');
+    }
+  }
+  
+  handleChange = e => {
+    
+    e.preventDefault();
+    const { name, value } = e.target;
+    let errors = this.state.errors;
+    
+    switch (name) {
+      case 'password':
+        pass = value;
+        if (passRegex.test(value) != true) {
+          errors.password = "Minimum 6 characters required with at least 1 number, 1 letter, and 1 special character";
+        }
+        
+        else {
+          errors.password ="";
+        }
+    
+      break;
+      case 'confirmPassword':
+        confirm = value;
+        if (confirm !== pass){
+          errors.confirmPassword = "Passwords do not match";
+        }
+        else {
+          errors.confirmPassword = "";
+        }
+      break;
+      default:
           break;
       }
-
       this.setState({errors, [name]: value}, () => console.log(this.state));
+      console.log(keyValue)
+      console.log(finalURL)
     };
 
     
@@ -186,11 +164,8 @@ class resetPassword extends Component {
                     )}            
                 </div>
                 
-                {this.state.apiError && (
-                    <span className="error">{this.state.apiError}<br></br><br></br></span>
-                )} 
                 {this.state.successMessage && (
-                    <span className="success">{this.state.successMessage}<br></br><br></br></span>
+                    <span className="error">{this.state.successMessage}<br></br><br></br></span>
                 )} 
 
                 <button type="submit" className="register-button">Reset Password</button>
